2d138a27ecdc37cfbe57a97e3c21608680233930
Sayema Qureshi <qureshisayema47@gmail.com>
2025-08-06 20:04:29 +0530

    new concept learned

---
+++ codewithharry/format_and_doc_string.py
@@ -0,0 +1,15 @@
+#using format in print in order to access var efficiently
+
+#USE OF DOCSTRING
+def make_tea():
+    '''recipe to make a refreshing tea'''
+    print("Boil water") #put the water in pan and wait until it boils
+    print("Add tea leaves")
+    print("Add milk and sugar")
+    print("serve it in a cup")
+
+make_tea()
+
+'''In above code the doc string is the line just before function definition'
+but the line on the first line is called a comment '''
+
