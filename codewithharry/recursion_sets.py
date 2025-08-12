183f80960d6c7a9e8ffc0f72070e15bea21b4d64
Sayema Qureshi <qureshisayema47@gmail.com>
2025-08-12 21:45:11 +0530

    hello

--- codewithharry/recursion_sets.py
+++ codewithharry/recursion_sets.py
@@ -130,6 +130,19 @@ for word in sentence.split():
         num_a_or_e += 1
 print(num_a_or_e)
 
+''' Q15] Write code that will count the number of vowels in the sentence s and assign the result to the variable num_vowels.
+For this problem, vowels are only a, e, i, o, and u. Hint: use the in operator with vowels.'''
+
+s = "singing in the rain and playing in the rain are two entirely different situations but both can be fun"
+vowels = ['a','e','i','o','u']
+
+vowels = ['a','e','i','o','u']
+num_vowels = 0
+for char in s:
+    if char in vowels:
+        num_vowels += 1
+print(num_vowels)
+
 
 
     
