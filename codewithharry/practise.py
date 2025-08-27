5914e36731e53d33554ad49721a92d350c9321b8
Sayema Qureshi <qureshisayema47@gmail.com>
2025-08-27 22:23:49 +0530

    pratice

--- codewithharry/practise.py
+++ codewithharry/practise.py
@@ -1,8 +1,15 @@
-#1]
-print("Welcome to the Coding Village!")
+# #1]
+# print("Welcome to the Coding Village!")
 
-#2]
-name = input("What is your name, adventurer?: ")
+# #2]
+# name = input("What is your name, adventurer?: ")
 
-#3]
-print(f"Greetings, {name} Your journey begins now.")
+# #3]
+# print(f"Greetings, {name} Your journey begins now.")
+
+#4]
+print("Welcome, adventurer!")
+favcolor = input("what is your fav color adventurer: ")
+print(f"Ah, so your favorite color is {favcolor}. Interesting choice!")
+
+
