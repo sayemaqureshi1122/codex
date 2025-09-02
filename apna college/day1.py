9b43d000a28bbccc2bd91cee0caec2eea27a2961
Sayema Qureshi <qureshisayema47@gmail.com>
2025-09-02 08:02:18 +0530

    l

--- apna college/day1.py
+++ apna college/day1.py
@@ -12,29 +12,41 @@
 # print("result is : " + str(first + second))
 # print(super_hero.find('T'))
 
-#building a calculator
-num1 = int(input())
-num2 = int(input())
-operation = input("enter operation u want to perform: (ie. sum, subtract, multiplication, division, power, mod): ")
-def cal(num1, num2, operation):
-    if operation == "sum":
-        print(num1 + num2)
-    elif operation == "subtract":
-        print(num1 -  num2)
-    elif operation == "division":
-        if num2 > 0:
-            print(num1 / num2)
-        else:
-            print("division with 0 is invalid")
-    elif operation == "multiplication":
-        print(num1 * num2)
-    elif operation == "mod":
-        if num2 > 0:
-            print(num1 // num2)
-        else:
-            print("division with zero is invalid")
-    elif operation == "power":
-        print(num1 ** num2)
-    else:
-        print("invalid operation")
-cal(num1, num2, operation)
+# #building a calculator
+# num1 = int(input("enter num1: "))
+# num2 = int(input("enter num2: "))
+# operation = input("enter operation u want to perform: (ie. +, -, *, /, //, **, %): ")
+# def cal(num1, num2, operation):
+#     if operation == "+":
+#         print(num1 + num2)
+#     elif operation == "-":
+#         print(num1 -  num2)
+#     elif operation == "/":
+#         if num2 > 0:
+#             print(num1 / num2)
+#         else:
+#             print("division with 0 is invalid")
+#     elif operation == "*":
+#         print(num1 * num2)
+#     elif operation == "//":
+#         if num2 > 0:
+#             print(num1 // num2)
+#         else:
+#             print("division with zero is invalid")
+#     elif operation == "**":
+#         print(num1 ** num2)
+#     elif operation == "%":
+#         print(num1 % num2)
+#     else:
+#         print("invalid operation")
+# cal(num1, num2, operation)
+
+#patterns using for loop
+
+i = 1
+num = int(input())
+while i <= num:
+    print(i * "*")
+    i += 1
+    
+   
