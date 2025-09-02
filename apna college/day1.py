4659469656b06e0f683ad169483b963c286e04d0
Sayema Qureshi <qureshisayema47@gmail.com>
2025-09-02 07:17:24 +0530

    hi

---
+++ apna college/day1.py
@@ -0,0 +1,13 @@
+first_name = "tony"
+last_name = "stark"
+age = 51
+is_genius = True
+print(first_name, last_name, is_genius)
+
+super_hero = input("what is your superhero name? ")
+print("Hello " + super_hero)
+
+first = int(input("enter a num: "))
+second = int(input("enter a num: "))
+print("result is : " + str(first + second))
+print(super_hero.find('T'))
