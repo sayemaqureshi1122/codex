10de97f6bfa45d0bfbb43ae1b3449fcf0392fa0a
Sayema Qureshi <qureshisayema47@gmail.com>
2025-09-07 11:59:52 +0530

    final

--- training/day4.py
+++ training/day4.py
@@ -21,34 +21,34 @@ for i in n:
         print("fizzbuzz")
         
         
-# marks=int(input("enter marks:"))
-# if marks >=60:
-#     if marks>=70:
-#         if marks>=80:
-#             if marks>=90:
-#                 print("A")
-#             else:
-#                 print("B")
-#         else:
-#             print("C")
-#     else:
-#         print("D")
-# else:
-#     print("fail")
+marks=int(input("enter marks:"))
+if marks >=60:
+    if marks>=70:
+        if marks>=80:
+            if marks>=90:
+                print("A")
+            else:
+                print("B")
+        else:
+            print("C")
+    else:
+        print("D")
+else:
+    print("fail")
 
-# a=1
-# b=2
-# c=3
-# if a > b and a>c:
-#     print(a)
-# if b > a and b > c:
-#     print(b)
-# if c > a and c > a:
-#     print(c)
+a=1
+b=2
+c=3
+if a > b and a>c:
+    print(a)
+if b > a and b > c:
+    print(b)
+if c > a and c > a:
+    print(c)
     
-# year=2000
-# if (year%4 ==0 and year%100 !=0) or (year%400==0):
-#     print(year, " is a leap year")
-# else:
-#     print( year,"not a leap year")
+year=2000
+if (year%4 ==0 and year%100 !=0) or (year%400==0):
+    print(year, " is a leap year")
+else:
+    print( year,"not a leap year")
 
