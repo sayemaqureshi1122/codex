65ea3e28d623872152f6ad6c52f076f0beff2d3f
Sayema Qureshi <qureshisayema47@gmail.com>
2025-09-07 11:59:07 +0530

    good morning

--- training/day4.py
+++ training/day4.py
@@ -1,15 +1,15 @@
-# #to count no of even numbers in an array
-# arr=[1,2,3,4,5]
-# c=0 
-# d=0
-# for i in arr:
-#      if i%2 == 0:
-#          c +=1
-#      if i in arr:
-#          if i%2 !=0:
-#              d+=1
-# print(c)
-# print(d)
+#to count no of even numbers in an array
+arr=[1,2,3,4,5]
+c=0 
+d=0
+for i in arr:
+     if i%2 == 0:
+         c +=1
+     if i in arr:
+         if i%2 !=0:
+             d+=1
+print(c)
+print(d)
 
 # n=[1,2,3,4]
 # for i in n:
@@ -21,34 +21,34 @@
 #         print("fizzbuzz")
         
         
-marks=int(input("enter marks:"))
-if marks >=60:
-    if marks>=70:
-        if marks>=80:
-            if marks>=90:
-                print("A")
-            else:
-                print("B")
-        else:
-            print("C")
-    else:
-        print("D")
-else:
-    print("fail")
+# marks=int(input("enter marks:"))
+# if marks >=60:
+#     if marks>=70:
+#         if marks>=80:
+#             if marks>=90:
+#                 print("A")
+#             else:
+#                 print("B")
+#         else:
+#             print("C")
+#     else:
+#         print("D")
+# else:
+#     print("fail")
 
-a=1
-b=2
-c=3
-if a > b and a>c:
-    print(a)
-if b > a and b > c:
-    print(b)
-if c > a and c > a:
-    print(c)
+# a=1
+# b=2
+# c=3
+# if a > b and a>c:
+#     print(a)
+# if b > a and b > c:
+#     print(b)
+# if c > a and c > a:
+#     print(c)
     
-year=2000
-if (year%4 ==0 and year%100 !=0) or (year%400==0):
-    print(year, " is a leap year")
-else:
-    print( year,"not a leap year")
+# year=2000
+# if (year%4 ==0 and year%100 !=0) or (year%400==0):
+#     print(year, " is a leap year")
+# else:
+#     print( year,"not a leap year")
 
