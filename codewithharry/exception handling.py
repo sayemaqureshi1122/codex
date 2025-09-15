6ba7c04ec4c7722308dd363db3314fab074bb3b8
Sayema Qureshi <qureshisayema47@gmail.com>
2025-09-15 12:32:31 +0530

    new concept

--- codewithharry/exception handling.py
+++ codewithharry/exception handling.py
@@ -10,12 +10,21 @@
 #     T -= 1
     
     
-n = input("enter a num: ")
-try:
-    for i in range(1,11):
-        print(f"{int(n)} X {i} = {int(n)*i}")
-except ValueError :
-    print("invalid input")
-    
+# n = input("enter a num: ")
+# try:
+#     for i in range(1,11):
+#         print(f"{int(n)} X {i} = {int(n)*i}")
+# except ValueError :
+#     print("invalid input")
 
-    
+
+#FINALLY CLAUSE
+
+list = [1, 2, 3, 4]
+try:
+    i = int(input("enter a index: "))
+    print(list[i])    
+except:
+    print("error occurs")
+finally:
+    print("i am always executed")
