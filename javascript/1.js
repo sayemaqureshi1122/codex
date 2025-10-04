1783bb463b0ab90e9a8bf1f619218ea06d1ac08a
Sayema Qureshi <qureshisayema47@gmail.com>
2025-10-04 14:06:12 +0530

    Q3 done

--- javascript/1.js
+++ javascript/1.js
@@ -11,9 +11,9 @@ function search(arr, element) {
 let arr = [1, 3, 7, 43, 45, 2, 8];
 let res = search(arr, 45);
 console.log(res);
-*/
+
 //Q2] write a function that return the number of negative numbers in an array.
-function find_negative_no(arr){
+function countnegative(arr){
     let sum = 0;
     for(let i = 0; i < arr.length; i++){
         if(arr[i] < 0){
@@ -24,5 +24,23 @@ function find_negative_no(arr){
 }
 
 let arr = [1, -4, 5, -5, -7, -9, 0, -3];
-let ans = find_negative_no(arr);
+let ans = countnegative(arr);
+console.log(ans);
+*/
+//Q3] write a function that returns the largest number in an array.
+
+function findlargest(arr){
+    for( let i = 0; i < arr.length; i++){
+        for( let j = 0; j < arr. length; j++){
+            if(arr[j] > arr[j+1]){
+                let stack = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = stack;
+            }
+        }
+    }
+    return arr;
+}
+let arr = [1, 4, 5, 8, 7];
+let ans = findlargest(arr);
 console.log(ans);
