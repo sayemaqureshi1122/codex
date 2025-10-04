e8be60f802d4ab54fef2c3d6f0e59ac9b1e9d903
Sayema Qureshi <qureshisayema47@gmail.com>
2025-10-04 13:45:36 +0530

    Q2 done

--- javascript/1.js
+++ javascript/1.js
@@ -1,3 +1,4 @@
+/*
 //Q1] write a function that searches an element in an array and returns the index and if the element is not present return -1.
 function search(arr, element) {
     for (let i = 0; i < arr.length; i++) {
@@ -10,3 +11,18 @@ function search(arr, element) {
 let arr = [1, 3, 7, 43, 45, 2, 8];
 let res = search(arr, 45);
 console.log(res);
+*/
+//Q2] write a function that return the number of negative numbers in an array.
+function find_negative_no(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < 0){
+            sum++;
+        }
+    }
+    return sum;
+}
+
+let arr = [1, -4, 5, -5, -7, -9, 0, -3];
+let ans = find_negative_no(arr);
+console.log(ans);
